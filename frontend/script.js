d5565fe87f2952373ab155f43f36b470abd8398d
Nandini P <nandini.pansare16@gmail.com>
2026-07-22 18:45:15 +0530

    Send from and to values in URL

--- frontend/script.js
+++ frontend/script.js
@@ -243,7 +243,7 @@ window.getAttendanceRange = async function(){
         if(response.ok){
             let display = data.message;
             if(data.data !== undefined){
-                display += `\nRecords: \n${data.data, null, 2}`;
+                display += `\nRecords: \n${data.data}`;
             }
             document.getElementById("attendanceRangeResult").textContent = display;
         }
