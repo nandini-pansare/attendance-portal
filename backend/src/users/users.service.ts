f1b8654e12b80802ee0a31302ab3c0c6d094d2ed
Nandini P <nandini.pansare16@gmail.com>
2026-07-24 15:47:51 +0530

    edit registration flow

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,7 +2,6 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './user.model';
 import * as bcrypt from 'bcrypt';
-import { Op, UniqueConstraintError } from 'sequelize';
 import { UserRole } from 'src/common/enums/role.enum';
 import { OtpService } from 'src/otp/otp.service';
 
@@ -15,46 +14,46 @@ export class UsersService {
     ){}
 
     async register(username: string, email: string, password: string, codeInput: number, otpInput: number){
-        console.log('USER SERVICE HIT');
-        
-        const code = Number(codeInput);
-        const otp = Number(otpInput);
-
-        if(!username || !email || !password || !codeInput || !otpInput){
-            throw new BadRequestException('Missing registration fields');
-        }
-
-        const existingEmail = await this.userModel.findOne({ where: {email}});
-        if(existingEmail){
-            throw new BadRequestException('Email already registered!');
-        }
-
-        const existingUsername = await this.userModel.findOne({ where: {username}});
-        if(existingUsername){
-            throw new BadRequestException('Username already taken!');
-        }
-
-        console.log('USER EMAIL AND USERNAME VALIDATED');
-
-        const verifyOtp = await this.otpService.verifyOtp(email, otp);
-        if(!verifyOtp){
-            throw new BadRequestException('Invalid or expired OTP');
-        }
-        console.log('OTP VERIFIED');
-        const hashedPassword = await bcrypt.hash(password, 10);
-        
-        let role: UserRole;
-        if(code === 9876){
-            role = UserRole.EMPLOYEE;
-        } else if(code === 8765){
-            role = UserRole.HR;
-        } else if(code === 7654){
-            role = UserRole.MANAGER;
-        } else{
-            throw new BadRequestException('Invalid Registration Code.');
-        }
-        
         try{
+            console.log('USER SERVICE HIT');
+        
+            const code = Number(codeInput);
+            const otp = Number(otpInput);
+
+            if(!username || !email || !password || !codeInput || !otpInput){
+                throw new BadRequestException('Missing registration fields');
+            }
+
+            const existingEmail = await this.userModel.findOne({ where: {email}});
+            if(existingEmail){
+                throw new BadRequestException('Email already registered!');
+            }
+
+            const existingUsername = await this.userModel.findOne({ where: {username}});
+            if(existingUsername){
+                throw new BadRequestException('Username already taken!');
+            }
+
+            console.log('USER EMAIL AND USERNAME VALIDATED');
+
+            const verifyOtp = await this.otpService.verifyOtp(email, otp);
+            if(!verifyOtp){
+                throw new BadRequestException('Invalid or expired OTP');
+            }
+            console.log('OTP VERIFIED');
+            const hashedPassword = await bcrypt.hash(password, 10);
+    
+            let role: UserRole;
+            if(code === 9876){
+                role = UserRole.EMPLOYEE;
+            } else if(code === 8765){
+                role = UserRole.HR;
+            } else if(code === 7654){
+                role = UserRole.MANAGER;
+            } else{
+                throw new BadRequestException('Invalid Registration Code.');
+            }
+        
             await this.userModel.create({ username, email, password: hashedPassword, role});
             return {
                 message: `${role} User Created.`,
