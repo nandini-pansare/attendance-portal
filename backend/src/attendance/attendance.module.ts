022e76e06f2292be6c0489d00641248e5c01a806
Nandini P <nandini.pansare16@gmail.com>
2026-08-10 16:27:38 +0530

    process create record section

--- backend/src/attendance/attendance.module.ts
+++ backend/src/attendance/attendance.module.ts
@@ -4,7 +4,6 @@ import { AttendanceService } from './attendance.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AttendanceModel } from './attendance.model';
 import { User } from 'src/users/user.model';
-import { JwtService } from '@nestjs/jwt';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -4,11 +4,26 @@ import { UsersController } from './users.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './user.model';
 import { OtpModule } from 'src/otp/otp.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ SequelizeModule.forFeature([User]), OtpModule],
+  imports: [
+    SequelizeModule.forFeature([User]),
+    OtpModule,
+    AuthModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
-  exports: [SequelizeModule]
+  exports: [SequelizeModule, JwtModule],
 })
 export class UsersModule {}
